1a9153edb34ae41c3f78b035799bf5b0736fca4d
4nth0ny1 <acatullo4@gmail.com>
2023-09-08 22:37:46 -0500

    added controllers and tested in postman

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
 const { logEvents } = require("./middleware/logger");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 connectDB();
 
@@ -25,6 +25,9 @@ app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/notes", require("./routes/noteRoutes"));
+
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
